bb9e171d00f3169045f9e514ac8a457a944db26a
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-03-11 16:47:35 +0100

    Correct spelling error

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -9,7 +9,7 @@ export default function Overlay(props: Props) {
     <div className="fixed bg-gray-300 bg-opacity-70 top-0 right-0 bottom-0 left-0 z-20 flex justify-center items-center">
       <div className="bg-white flex flex-wrap justify-center items-center rounded-lg h-60 p-10">
         <h3 className="text-2xl w-full text-center">
-          The Product has been added to the cart
+          The product has been added to the cart
         </h3>
 
         <Link href="/cart">
