43c8d954e8fa251b915168fbe154fc98936dcee0
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-03-11 09:53:40 +0100

    Configure for deployment to Heroku

--- util/database.ts
+++ util/database.ts
@@ -1,9 +1,29 @@
 import camelcaseKeys from 'camelcase-keys';
+import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku';
 import { Chocolate } from './types';
 const postgres = require('postgres');
 require('dotenv-safe').config();
 
-const sql = postgres();
+setPostgresDefaultsOnHeroku();
+
+function connectOneTimeToDatabase() {
+  let sql;
+
+  if (process.env.NODE_ENV === 'production') {
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.__postgresSqlClient) {
+      globalThis.__postgresSqlClient = postgres();
+    }
+    sql = globalThis.__postgresSqlClient;
+  }
+  return sql;
+}
+
+const sql = connectOneTimeToDatabase();
 
 function camelcaseRecords(records: Chocolate[]) {
   return records.map((record) => camelcaseKeys(record));
