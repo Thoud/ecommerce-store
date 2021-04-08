475e011a9da91ab649ce589f54b98fc2f8e12025
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-04-08 09:59:25 +0200

    Invoke function to remove expired sessions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import Layout from '../components/Layout';
 import store from '../store/store';
 import '../styles/globals.css';
+import { deleteAllExpiredSessions } from '../util/database';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -16,6 +17,8 @@ export default function App({ Component, pageProps }: AppProps) {
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
+  await deleteAllExpiredSessions();
+
   if (
     context.req.headers.host &&
     context.req.headers['x-forwarded-proto'] &&
